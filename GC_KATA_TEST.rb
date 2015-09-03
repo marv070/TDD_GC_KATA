187f6a631b38c0e55b2c9760df73b0dd0cdf0590
Marvin Laucher <marv070@gmail.com>
2015-09-03 11:59:07 -0400

    added test for first element in array having value of 1

--- GC_KATA_TEST.rb
+++ GC_KATA_TEST.rb
@@ -7,4 +7,17 @@ class GC_kate_test < Minitest::Test
     array = greene_county
     assert_equal(100, array.length)
   end
+
+  def test_first_element_in_array_1
+  	results = greene_county
+  	assert_equal(1, results[0])
+  end
+
+
+
+
+
+
+
+
 end
